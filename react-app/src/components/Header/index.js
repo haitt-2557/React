53daf0d8740978b5c17c57a6887cc70b5af83076
haitt-2557 <truong.thanh.hai@sun-asterisk.com>
2021-08-10 07:51:19 +0700

    sort, search

--- react-app/src/components/Header/index.js
+++ react-app/src/components/Header/index.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Logo from "../../assets/imgs/logo-is.webp";
+import axiosClient from "../../untils/axiosClient";
+import { TypesSuccess } from "../../constants/types";
 import "./style.scss";
 
 export default function Header() {
+  const typingTimeoutRef = useRef(null);
+  const filters = useSelector((state) => state.products.filters);
+  const dispatch = useDispatch();
+
+  const handleSearchChange = (e) => {
+    const { value } = e.target;
+
+    if (typingTimeoutRef.current) {
+      clearTimeout(typingTimeoutRef.current);
+    }
+
+    typingTimeoutRef.current = setTimeout(() => {
+      onSearch(value);
+    }, 500);
+  };
+
+  const onSearch = async (value) => {
+    try {
+      const payload = {
+        ...filters,
+        q: value,
+      };
+      const { data } = await axiosClient.get("products", {
+        params: payload,
+      });
+      dispatch({
+        type: TypesSuccess.SEARCH_PRODUCT_SUCCESS,
+        payload: { products: data, filters: payload },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <header id="header">
       <div className="header__container">
@@ -22,6 +58,7 @@ export default function Header() {
               autoCorrect="off"
               placeholder="Search a product"
               spellCheck="false"
+              onChange={handleSearchChange}
             />
           </div>
           <span className="input-group-btn">
--- react-app/src/components/MainContainer/Sort.js
+++ react-app/src/components/MainContainer/Sort.js
@@ -1,25 +1,56 @@
-import React from 'react';
+import { TypesSuccess } from "../../constants/types";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axiosClient from "../../untils/axiosClient";
 
 function Sort() {
-	return (
-		<div className='sort__container'>
-			<span className='sort-infor'>123 results found in 123ms</span>
-			<div className='sort-action'>
-				<span className='mr-2'>Sort by</span>
-				<select className='ais-sort-by-selector'>
-					<option className='ais-sort-by-selector--item' value=''>
-						Featured
-					</option>
-					<option className='ais-sort-by-selector--item' value='asc'>
-						Price asc.
-					</option>
-					<option className='ais-sort-by-selector--item' value='desc'>
-						Price desc.
-					</option>
-				</select>
-			</div>
-		</div>
-	);
+  const dispatch = useDispatch();
+  const { filters, panigations } = useSelector((state) => state.products);
+
+  const handleSort = async (e) => {
+    try {
+      const orderBy = e.target.value;
+      const payload = {
+        ...filters,
+        _sort: "price",
+        _order: orderBy,
+      };
+      if (!orderBy) {
+        delete payload._order;
+        delete payload._sort;
+      }
+      const { data } = await axiosClient.get("products", { params: payload });
+      dispatch({
+        type: TypesSuccess.SEARCH_PRODUCT_SUCCESS,
+        payload: { products: data, filters: payload },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="sort__container">
+      <span className="sort-infor">
+        {panigations?.total} results found in 123ms
+      </span>
+      <div className="sort-action">
+        <span className="mr-2">Sort by</span>
+        <select className="ais-sort-by-selector" onChange={handleSort}>
+          {" "}
+          <option className="ais-sort-by-selector--item" value="">
+            Featured
+          </option>
+          <option className="ais-sort-by-selector--item" value="asc">
+            Price asc.
+          </option>
+          <option className="ais-sort-by-selector--item" value="desc">
+            Price desc.
+          </option>
+        </select>
+      </div>
+    </div>
+  );
 }
 
 export default Sort;
